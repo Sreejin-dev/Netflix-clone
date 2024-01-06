72c316df5d707b8ec0ae511b0b591f04051ba3ae
Sreejin-dev <sreejincp84@gmail.com>
2024-01-06 21:42:30 +0530

    navbar uptodate

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/https://github.com/Sreejin-dev/Netflix-clone/',
   plugins: [react()],
 })
